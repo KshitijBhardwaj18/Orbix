48e0ee647492416c37395a0a1cb45f6370940e7a
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2025-09-09 14:44:16 +0530

    add: (client) added tradewindow=

--- client/components/trade/TradingWindow.tsx
+++ client/components/trade/TradingWindow.tsx
@@ -9,14 +9,21 @@ interface TradeProps {
 }
 
 const Trade = ({ ticker }: { ticker: string }) => {
-  const [orderType, setOrderType] = useState("");
-  const [price, setPrice] = useState("");
-  const [quantity, setQuantity] = useState("");
+  const [orderType, setOrderType] = useState<string>("buy");
+  const [price, setPrice] = useState<number>(0);
+  const [quantity, setQuantity] = useState<number>(0);
+  const [orderValue, setOrderValue] = useState<number>(0);
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    s;
+    console.log({ orderType, price, quantity });
+  };
 
   return (
     <div className="w-full  bg-primary rounded-xl p-4">
-      <div className="flex flex-col gap-2">
-        <div className="flex flex-row bg-neutral-7 rounded-xl bg-[#202126] font-bold text-neutral-400">
+      <div className="flex flex-col gap-4 text-neutral-400">
+        <div className="flex flex-row bg-neutral-7 rounded-xl bg-[#202126] font-bold ">
           <button
             className={cn(
               "flex items-center justify-center p-4  rounded-xl   flex-1 hover:text-green-400",
@@ -36,6 +43,85 @@ const Trade = ({ ticker }: { ticker: string }) => {
             {" "}
             Sell
           </button>
+        </div>
+
+        <div className="flex flex-col ">
+          <form className="flex flex-col gap-6" onSubmit={onSubmit}>
+            <div className="flex flex-col  justify-start gap-2 ">
+              <label className="text-sm">Price</label>
+
+              <div className="relative flex bg-[#202126] flex-1  rounded-xl ">
+                <img
+                  className="size-7 absolute right-4 top-2"
+                  alt="usd"
+                  src={"/symbols/usd.svg"}
+                />
+                <input
+                  type="number"
+                  className="p-3 w-full pr-12 [appearance:textfield] 
+                  [&::-webkit-outer-spin-button]:appearance-none 
+                  [&::-webkit-inner-spin-button]:appearance-none rounded-xl"
+                  placeholder="0"
+                  value={price}
+                  onChange={(e) => setPrice(Number(e.target.value))}
+                ></input>
+              </div>
+            </div>
+
+            <div className="flex flex-col  justify-start gap-2 ">
+              <label className="text-sm">Quantity</label>
+
+              <div className="relative flex bg-[#202126] flex-1  rounded-xl ">
+                <img
+                  className="size-7 absolute right-4 top-2"
+                  alt="usd"
+                  src={"/symbols/btc.webp"}
+                />
+                <input
+                  type="number"
+                  className="p-3 w-full pr-12 [appearance:textfield] 
+                  [&::-webkit-outer-spin-button]:appearance-none 
+                  [&::-webkit-inner-spin-button]:appearance-none rounded-xl"
+                  placeholder="0"
+                  value={quantity}
+                  onChange={(e) => setQuantity(Number(e.target.value))}
+                ></input>
+              </div>
+            </div>
+
+            <div className="flex flex-col  justify-start gap-2 ">
+              <label className="text-sm">Order Value</label>
+
+              <div className="relative flex bg-[#202126] flex-1  rounded-xl ">
+                <img
+                  className="size-7 absolute right-4 top-2"
+                  alt="usd"
+                  src={"/symbols/usd.svg"}
+                />
+                <input
+                  type="number"
+                  className="p-3 w-full pr-12 [appearance:textfield] 
+                  [&::-webkit-outer-spin-button]:appearance-none 
+                  [&::-webkit-inner-spin-button]:appearance-none rounded-xl"
+                  placeholder="0"
+                  value={orderValue}
+                  onChange={(e) => setOrderValue(Number(e.target.value))}
+                ></input>
+              </div>
+            </div>
+
+            <div className="flex flex-1 items-center justify-center">
+              <button
+                className={cn(
+                  "cursor-pointer flex items-center justify-center flex-1 bg-white text-black py-2  shadow-xl rounded-xl",
+                  orderType == "buy" && "hover:bg-green-400 hover:text-white",
+                  orderType == "sell" && "hover:bg-red-500 hover:text-white"
+                )}
+              >
+                Place Order
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
